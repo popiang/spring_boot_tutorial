bb4285a691fba071acd0be73bac10ed0c92911a7
Shahril <popiang@hotmail.com>
2019-06-25 01:27:18 +0800

    progress

---
+++ src/main/java/com/popiang/model/SiteUser.java
@@ -0,0 +1,49 @@
+package com.popiang.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class SiteUser {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private long id;
+	
+	@Column(name = "email", unique = true)
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+		
+}
