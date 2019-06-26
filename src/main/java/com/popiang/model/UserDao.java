fe10d08b3934b4a58673eeacb10b494756dcb032
Shahril Bin Mad Shah <shahril_ma@airselangor.local>
2019-06-26 17:23:57 +0800

    more progress

---
+++ src/main/java/com/popiang/model/UserDao.java
@@ -0,0 +1,11 @@
+package com.popiang.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends CrudRepository<SiteUser, Long> {
+
+	SiteUser findByEmail(String email);
+	
+}
